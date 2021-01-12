ae84c3d280828a1e5ff34d4ebf19c510c35f962c
l.yakubenko <l.yakubenko@rabota.ru>
2021-01-12 18:18:04 +0300

    рефакторинг

--- check-availability.js
+++ check-availability.js
@@ -30,6 +30,27 @@ const sendEmail = ({text, subject}) => {
     });
 }
 
+const checkButton = async ({page, link, item}) => {
+    console.log('Проверяем кнопку')
+    const container = await page.$('.o-pay__content')
+    const buttons = await container.$$eval(
+        '.add-to-basket-button',
+        nodes => nodes.map(n => n.getAttribute("value")).filter(v => v === 'Добавить в корзину')
+    )
+
+    console.log(buttons)
+
+    if (buttons.length) {
+        console.log('Есть кнопка!')
+        sendEmail({text: `ссылка: ${link}`, subject: `${item} появилась в наличии!`})
+        // как только появится кнопка отправить письмо на почту...
+        return true
+    }
+    console.log('Кнопки нет :(')
+    return false
+}
+
+
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -41,40 +62,21 @@ const sendEmail = ({text, subject}) => {
         iphone: 'https://www.mvideo.ru/products/smartfon-apple-iphone-12-128gb-green-mgjf3ru-a-30052894'
     }
 
-    const checkButton = async ({link, item}) => {
-        console.log('Проверяем кнопку')
-        const container = await page.$('.o-pay__content')
-        const buttons = await container.$$eval(
-            '.add-to-basket-button',
-            nodes => nodes.map(n => n.getAttribute("value")).filter(v => v === 'Добавить в корзину')
-        )
-
-        console.log(buttons)
-
-        if (buttons.length) {
-            console.log('Есть кнопка!')
-            sendEmail({text: `ссылка: ${link}`, subject: `${item} появилась в наличии!`})
-            // как только появится кнопка отправить письмо на почту...
-            return true
-        }
-        console.log('Кнопки нет :(')
-        return false
-    }
-
     let result
 
     while (!result) {
+
         console.log('Заходим на пс5 диджитал')
         await page.goto(links.ps5Digital);
         await page.screenshot({path: 'ps5-digital.png', fullPage: true});
 
-        result = await checkButton({link: links.ps5Digital, item: 'PS5 digital edition'})
+        result = await checkButton({page, link: links.ps5Digital, item: 'PS5 digital edition'})
 
         console.log('Заходим на пс5 версию с дисководом')
         await page.goto(links.ps5)
         await page.screenshot({path: 'ps5.png', fullPage: true});
 
-        result = await checkButton({link: links.ps5, item: 'PS5 с дисководом'})
+        result = await checkButton({page, link: links.ps5, item: 'PS5 с дисководом'})
 
         // тест
         // console.log('Заходим на какой-то смартфон')
@@ -82,6 +84,7 @@ const sendEmail = ({text, subject}) => {
         // await page.screenshot({path: 'phone.png', fullPage: true});
         //
         // result = await checkButton({link: links.iphone, item: 'Iphone'})
+
     }
 
     await browser.close();
